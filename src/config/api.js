fe92673701d831822e8a47baf260b8901d6cd0ba
nhthi <nguyenhungthinh574@gmail.com>
2024-12-28 19:27:03 +0700

    test

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const API_BASE_URL = "http://localhost:8080";
+// export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://social-backend-1-90on.onrender.com";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
